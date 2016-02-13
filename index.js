f0fb663420e4f0516f2dbbcb6745dbe46e1ff2fd
akshay-nair <akshay.nair@programmer.net>
2016-02-13 11:01:25 -0500

    Add socket.io

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var socket = require('socket.io');
+var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
+var compression = require('compression');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,28 +14,26 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
-var cookieSession = require('cookie-session');
+
 app.use(cookieSession({
     keys: ['userId', 'userName']
 }));
 
-var compression = require('compression');
 app.use(compression());
-
-app.use(app.router);
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+var io = socket.listen(server);
+// TODO
